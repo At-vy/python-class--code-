ca862931ecc7b1b37f09f5acf57721acbe8b3e51
Atharva Vyas <vyasatharva7@gmail.com>
2026-06-24 23:02:53 +0530

    Delete calculator directory

--- calculator/firstmod.py
+++
@@ -1,6 +0,0 @@
-# using functions to farm a calculator 
-# addition and subtraction
-def add(a,b):
-    return a+b 
-def sub(a,b):
-    return a-b
--- calculator/secondmod.py
+++
@@ -1,6 +0,0 @@
-# using functions to farm a calculator 
-# division and multiplication
-def div(a,b):
-    return a/b
-def mul(a,b):
-    return a*b
